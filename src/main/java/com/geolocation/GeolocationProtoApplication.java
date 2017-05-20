99296bc577609679f3da00d4a1d159a1ff051da0
rekan <rekanagy06@gmail.com>
2017-05-20 22:48:41 +0200

    initial commit - basic project structure

---
+++ src/main/java/com/geolocation/GeolocationProtoApplication.java
@@ -0,0 +1,12 @@
+package com.geolocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolocationProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeolocationProtoApplication.class, args);
+	}
+}
