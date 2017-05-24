af1f48b71fa43cc4146541138293424576045c01
rekan <rekanagy06@gmail.com>
2017-05-24 11:04:38 +0200

    refactor and comments added

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -10,7 +10,7 @@ $(function() {
     var geolocationDataPanel = document.getElementById("geolocationData");
 
     function getLocation() {
-        if (navigator.geolocation) {;
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition, showError);
         } else {
             geolocationMapPanel.innerHTML = "Geolocation is not supported by this browser.";
@@ -51,16 +51,16 @@ $(function() {
     function showError(error) {
         switch(error.code) {
             case error.PERMISSION_DENIED:
-                x.innerHTML = "User denied the request for Geolocation."
+                geolocationDataPanel.innerHTML = "User denied the request for Geolocation."
                 break;
             case error.POSITION_UNAVAILABLE:
-                x.innerHTML = "Location information is unavailable."
+                geolocationDataPanel.innerHTML = "Location information is unavailable."
                 break;
             case error.TIMEOUT:
-                x.innerHTML = "The request to get user location timed out."
+                geolocationDataPanel.innerHTML = "The request to get user location timed out."
                 break;
             case error.UNKNOWN_ERROR:
-                x.innerHTML = "An unknown error occurred."
+                geolocationDataPanel.innerHTML = "An unknown error occurred."
                 break;
         }
     }
